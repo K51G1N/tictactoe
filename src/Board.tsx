8dff8989b27a8c3cc8cbe155a16583e70b0a4057
keagan <sir.ellenberger@gmail.com>
2024-02-29 16:42:03 +0200

    refactor: board using forloops

--- src/Board.tsx
+++ src/Board.tsx
@@ -56,23 +56,22 @@ export default function Board({xIsNext, squares, onPlay}: BoardProps){
     }
     
     return (
-        <div>
+        <div> 
             <div className="status">{status}</div>
-            <div className='board-row'>
-                < Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                < Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                < Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-            </div>
-            <div className='board-row'>
-                < Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                < Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                < Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-            </div>
-            <div className='board-row'>
-                < Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                < Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                < Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-            </div>
+            {Array.from({ length: 3 }).map((_, i: number) => (
+                <div className="board-row" key={i}>
+                    {Array.from({ length: 3 }).map((_, j: number) => {
+                        const index: number = i * 3 + j;
+                        return (
+                        <Square
+                            key={index}
+                            value={squares[index]}
+                            onSquareClick={() => handleClick(index)}
+                        />
+                        );
+                    })}
+                </div>
+            ))} 
         </div>
     )
 
